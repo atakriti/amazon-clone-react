e868fc420ebe96ce0471b94da5cd937ef1809549
atakriti <anwart256@gmail.com>
2022-07-02 17:19:02 +0200

    router done

--- src/App.js
+++ src/App.js
@@ -2,22 +2,34 @@ import Header from "./components/Header";
 import Baner from "./components/Baner";
 import Rows from "./components/Rows";
 import SignIn from "./components/SignIn";
-import { Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./components/banerCarts.scss"
 import "./main.scss"
 function App() {
   
-  return <div className="App">
-       
-  <Header/>
-  <Baner/>
-    <Rows />
-    
+  return  <div className="App">
+    < Router >
+      {/* Route SignIn */}
+    <Routes>
+    <Route path="/signIn" element={<SignIn />}/>
+      </Routes>
+      {/* Route Header */}
+      <Routes>
+        <Route path="/" element={<Header />} />
+      </Routes>
+      {/* Route Baner */}
         <Routes>
-        <Route path="signIn" element={ <SignIn />}/>        
+        <Route path="/" element={<Baner />} />
+      </Routes>
+      {/* Route Rows */}
+      <Routes>
+        <Route path="/" element={ <Rows/>}/>
         </Routes>
-    </div>
+    </Router>
+     
+  </div>
+  
     ;
 }
 
