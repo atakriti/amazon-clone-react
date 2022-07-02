6f8c193687c44fdc694d74efba62cc612dd5d8c3
atakriti <anwart256@gmail.com>
2022-07-02 15:54:01 +0200

    main carts done

--- src/components/Baner.js
+++ src/components/Baner.js
@@ -1,4 +1,5 @@
 import BanerCarts from "./BanerCarts"
+import BanerCartTwo from "./BanerCartTwo";
 import "./banerCarts.scss";
 
 let banerImg = "https://m.media-amazon.com/images/I/61jovjd+f9L._SX3000_.jpg";
@@ -12,6 +13,15 @@ let img1Link2 = "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExpor
 let img2Link2 = "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/August/DashboardCard/PS4_120X._SY85_CB438749318_.jpg"
 let img3Link2 = "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/February/Dashboard/Baby120X._SY85_CB468850882_.jpg"
 let img4Link2 = "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/February/Dashboard/Toys120X._SY85_CB468851693_.jpg"
+// third 
+let dressesImg = "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2022/February/DashboardCards/Fuji_dash_dress_1X._SY304_CB626369146_.jpg"
+let computersImg ="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_PC_1x._SY304_CB431800965_.jpg"
+let healthImg ="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2022/February/DashboardCards/GW_CONS_AUS_HPC_HPCEssentials_CatCard_Desktop1x._SY304_CB627424361_.jpg"
+let dealImg = "https://m.media-amazon.com/images/I/417+VAaLnXL._AC_SY230_.jpg"
+let electronicImg = "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_Electronics_1x._SY304_CB432774322_.jpg"
+
+let weShipImg = "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/October/Fuji_D2_45M_en_US_1x._CB418309979_.jpg"
+
 
 function Baner() {
     return (
@@ -45,6 +55,35 @@ function Baner() {
               text3="Computer mice"
               img4={img4Link2}
               text4 = "Chairs"          
+          />
+          {/* Dresses */}
+          <BanerCartTwo
+            h2="Dresses"
+            image={dressesImg}
+          />
+          {/* computers */}
+          <BanerCartTwo
+            h2="Computers & Accessories"
+            image={computersImg}
+          />
+          {/* Health */}
+          <BanerCartTwo
+            h2="Health & Personal Care"
+            image={healthImg}
+          />
+          {/* dealof the Day */}
+          <BanerCartTwo
+            h2="Deal of the Day"
+            image={dealImg}
+          />
+          {/* Electronic  */}
+          <BanerCartTwo
+            h2="Electronics"
+            image={electronicImg}
+          />
+           <BanerCartTwo
+            h2="We Ship over 45 milion"
+            image={weShipImg}
           />
             </div>
 
---
+++ src/components/BanerCartTwo.js
@@ -0,0 +1,11 @@
+function BanerCartTwo(props) {
+  return (
+    <div className="banerCartTwo">
+      <h2>{props.h2}</h2>
+      <img src={props.image} alt="" />
+      <a>Shop now</a>
+    </div>
+  );
+}
+
+export default BanerCartTwo;
