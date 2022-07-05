3654c8ca1ac323605de7c92e59c31a8d5c11050a
atakriti <anwart256@gmail.com>
2022-07-05 11:11:12 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import {Header} from "./components/Header";
 import Baner from "./components/Baner";
 import Rows from "./components/Rows";
 import SignIn from "./components/SignIn";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {useState} from "react"
 import "./components/banerCarts.scss"
 import "./main.scss"
 import Footer from "./components/Footer";
@@ -13,24 +13,37 @@ import Footer from "./components/Footer";
 
 
 function App() {
+
+
+  let [number, setNumber] = useState(0)
+
+  let handleIncrement = () => {
+    setNumber(number + 1)
+  }
   
   return  <div className="App">
       {/* Route SignIn */}
-    <Routes>
-        <Route path="/signIn" element={<SignIn />}/>
-      </Routes>
+      <SignIn />
+      <Header  zero={number}  />
+      <Baner />
+      <Rows handleCount={handleIncrement} />
       
+      <Footer/>
       {/* Route Header */}
 
-      <Header />
-      <Baner />
-      <Rows/>
-      <Footer/>
 
-
-
+      
      
-  </div>;
+      {/* Route Baner */}
+       
+      {/* Route Rows */}
+     
+        
+     
+  </div>
+   
+  
+    ;
   
 }
 
